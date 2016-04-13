5a75fab9e635f37819270973178775eb7c87e873
Tin Crnkovic <tinc2k@gmail.com>
2016-04-13 19:59:58 +0200

    Initial Atom config

---
+++ atom/install_packages.ps1
@@ -0,0 +1,3 @@
+
+# tip: use `apm list` to obtain a list of installed Atom packages
+apm install activate-power-mode atom-jade chester-atom-syntax flatten-json highlight-selected jshint nucleus-dark-ui
